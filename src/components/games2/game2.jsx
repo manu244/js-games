ee139193a1b6547f21da44528cf739db232d0ebf
Manish Kumar Vishwakarma <31726435+manu244@users.noreply.github.com>
2022-08-21 09:10:27 +0530

    changes pushed

--- src/components/games2/game2.jsx
+++ src/components/games2/game2.jsx
@@ -24,13 +24,14 @@ function Game2(props) {
           bounds="parent"
           handle=".handle"
           defaultPosition={{ x: 105, y: 112 }}
-          position={null}
-        >
+          position={null}>
           <div className="inline-block">
             <strong className="handle">
               <div className="s1">&#9769;</div>
             </strong>
-            <img src="./images/game2/g2s1.svg" alt="img" className="g2-img" />
+            <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 119 239" width="119" height="239">
+              <path id="Layer" fill= "#f0ad00" fill-rule="evenodd" class="s0" d="m118.9 0.7v237.7l-118.8-118.9z"/>
+            </svg>
           </div>
         </Draggable>
         <Draggable
@@ -43,7 +44,9 @@ function Game2(props) {
             <strong className="handle">
               <div className="s2">&#9769;</div>
             </strong>
-            <img src="./images/game2/g2s2.svg" alt="img" className="g2-img" />
+            <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 270 536" width="270" height="536">
+              <path id="Layer" fill="#5a6378" fill-rule="evenodd" class="s0" d="m0 536.4v-535.8l267.9 267.9z"/>
+            </svg>
           </div>
         </Draggable>
         <Draggable
@@ -56,20 +59,69 @@ function Game2(props) {
             <strong className="handle">
               <div className="s3">&#9769;</div>
             </strong>
-            <img src="./images/game2/g2s3.svg" alt="img" className="g2-img" />
+            <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 253 252" width="253" height="252">
+              <path id="Layer" fill="#60b5cc" fill-rule="evenodd" class="s0" d="m0.9 0h252v252z"/>
+            </svg>
           </div>
         </Draggable>
         <Draggable
           bounds="parent"
           handle=".handle"
-          defaultPosition={{ x: 151, y: 287 }}
+          defaultPosition={{ x: 0, y: 0 }}
           position={null}
         >
           <div className="inline-block">
             <strong className="handle">
               <div className="s4">&#9769;</div>
             </strong>
-            <img src="./images/game2/g2s4.svg" alt="img" className="g2-img" />
+            <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 395 124" width="395" height="124">
+              <path id="Layer copy" fill="#7fd13b" fill-rule="evenodd" class="s0" d="m270.8 0l123.4 123.4h-268.2l-123.5-123.4zm15.3 283.5l133.5 133.5 134.1-134.1-133.5-133.6z"/>
+            </svg>
+          </div>
+        </Draggable>
+        <Draggable
+          bounds="parent"
+          handle=".handle"
+          defaultPosition={{ x: 0, y: 0 }}
+          position={null}
+        >
+          <div className="inline-block">
+            <strong className="handle">
+              <div className="s5">&#9769;</div>
+            </strong>
+            <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 269 269" width="269" height="269">
+            <path id="Layer" fill="#7fd13b" fill-rule="evenodd" class="s0" d="m-14.2-148l123.4 123.4h-268.2l-123.5-123.4zm15.3 283.5l133.5 133.5 134.1-134.1-133.5-133.6z"/>
+          </svg>
+          </div>
+        </Draggable>
+        <Draggable
+          bounds="parent"
+          handle=".handle"
+          defaultPosition={{ x: 0, y: 0 }}
+          position={null}
+        >
+          <div className="inline-block">
+            <strong className="handle">
+              <div className="s6">&#9769;</div>
+            </strong>
+            <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 245 123" width="245" height="123">
+              <path id="Layer" fill="#f0ad00" fill-rule="evenodd" class="s0" d="m-0.1 0.4h245.1l-122.5 122.5z"/>
+            </svg>
+          </div>
+        </Draggable>
+        <Draggable
+          bounds="parent"
+          handle=".handle"
+          defaultPosition={{ x: 0, y: 0 }}
+          position={null}
+        >
+          <div className="inline-block">
+            <strong className="handle">
+              <div className="s7">&#9769;</div>
+            </strong>
+            <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 544 272" width="544" height="272">
+              <path id="Layer" fill="#60b5cc" fill-rule="evenodd" class="s0" d="m541.4 271.9h-535.8l267.9-267.9z"/>
+            </svg>
           </div>
         </Draggable>
       </div>
