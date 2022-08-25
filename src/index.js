e1ea2f4176952ef6536675c0c16ff0ca88269b55
Manish Kumar Vishwakarma <31726435+manu244@users.noreply.github.com>
2022-08-25 23:34:30 +0530

    code commented

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
